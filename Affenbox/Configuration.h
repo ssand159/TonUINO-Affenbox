e78129e15e3d365a177ebc5c348dd65c619ed97a
marco-117 <56135788+marco-117@users.noreply.github.com>
2022-04-13 21:23:33 +0000

    Update Configuration.h

--- Affenbox/Configuration.h
+++ Affenbox/Configuration.h
@@ -7,7 +7,7 @@
 *https://www.leiterkartenpiraten.de/produkt/tonuino-all-in-one/
 */
 
-#define AiO
+//#define AiO
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -28,21 +28,25 @@
 /** 
  * Konfiguration der Standard Button Pins 
  */
- 
-/**
-*Sample Classic
-*/
-//#define buttonPause A0
-//#define buttonUp A1 
-//#define buttonDown A2
-
-
+ #if defined AiO
 /**
 *Sample All in One
 */
+
  #define buttonPause A0
  #define buttonUp A4
  #define buttonDown A3
+
+#else
+/**
+*Sample Classic
+*/
+
+#define buttonPause A0
+#define buttonUp A1 
+#define buttonDown A2
+
+#endif
 
 /**
 *Sample Affenbox Pocket
