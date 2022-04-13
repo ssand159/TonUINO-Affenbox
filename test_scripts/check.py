16fad61a7dcc53fe2dd4cfc5e907bf3ebfbeda12
marco-117 <56135788+marco-117@users.noreply.github.com>
2022-04-13 21:54:56 +0000

    Update check.py

--- test_scripts/check.py
+++ test_scripts/check.py
@@ -42,7 +42,7 @@ void loop() {
 }
 """
 import serial, time, sys
-arduino = serial.Serial('/dev/ttyACM0', 115200, timeout=.1)
+arduino = serial.Serial('/dev/ttyUSB0', 115200, timeout=.1)
 time.sleep(1)
 arduino.reset_input_buffer()
 arduino.reset_output_buffer()
