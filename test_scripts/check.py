9ef54404a48ad9a1404ac20fcf088f4a4808127f
marco-117 <56135788+marco-117@users.noreply.github.com>
2022-04-14 20:50:25 +0000

    Update check.py

--- test_scripts/check.py
+++ test_scripts/check.py
@@ -2,6 +2,7 @@ import serial, time, sys
 arduino = serial.Serial('/dev/ttyUSB0', 115200, timeout=.1)
 arduino.reset_input_buffer()
 arduino.reset_output_buffer()
+arduino.close()
 arduino.open()
 time.sleep(1)
 while arduino.in_waiting() > 0:
