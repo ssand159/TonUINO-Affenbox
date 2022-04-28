07fdec6a833c3fa9e9b857dfdbe2dbb8d921ac6a
marco-117 <56135788+marco-117@users.noreply.github.com>
2022-04-28 20:34:16 +0000

    Update check.py
    fix serial print, now the first 50 incoming lines will be printed

--- test_scripts/check.py
+++ test_scripts/check.py
@@ -1,9 +1,18 @@
+from pickle import TRUE
 import serial, time, sys
-arduino = serial.Serial('/dev/ttyUSB0', 115200, timeout=.1)
+arduino = serial.Serial('COM11', 115200, timeout=.1)
 arduino.reset_input_buffer()
 arduino.reset_output_buffer()
 arduino.close()
 arduino.open()
-print (arduino.readlines())
-  
+sc = ''
+s = ""
+i = 1
+while i < 50:
+  s = arduino.readline()
+  if s :
+    print (s)
+  i += 1
+
+arduino.close()
 sys.exit(0)
